56bc055332286f6533a57cafc338f652e5eb4405
Song's <100990317+gyxii@users.noreply.github.com>
2022-03-18 00:51:22 +0900

    반응형 css

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
-
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove('open');
+  scrollIntoView(link);
+});
 
 // Navbar toggle button for small screen
 const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 navbarToggleBtn.addEventListener('click', () => {
   navbarMenu.classList.toggle('open');
 });
-
-
-
-
